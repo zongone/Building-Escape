8affa45003b3225295382718865d04e77cd66ac0
maciej <maciek@uxfactor.pl>
2017-08-02 22:32:24 +0200

    Class 62

--- BuildingEscape/Source/BuildingEscape/NewActorComponent.cpp
+++ BuildingEscape/Source/BuildingEscape/NewActorComponent.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PositionReport.h"
+#include "NewActorComponent.h"
 
 
 // Sets default values for this component's properties
-UPositionReport::UPositionReport()
+UNewActorComponent::UNewActorComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
@@ -15,19 +15,19 @@ UPositionReport::UPositionReport()
 
 
 // Called when the game starts
-void UPositionReport::BeginPlay()
+void UNewActorComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
 	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty on chair!"));
+	UE_LOG(LogTemp, Warning, TEXT("Your message"));
 
 	
 }
 
 
 // Called every frame
-void UPositionReport::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UNewActorComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
